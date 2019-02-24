8066d19a645539bf7524ca2abf9fb6cd4a3106a1
wgf18331165882 <18331165882@163.com>
2019-02-24 18:51:47 +0800

    112

---
+++ src/main/java/cn/itcast/travel/domain/A.java
@@ -0,0 +1,5 @@
+package cn.itcast.travel.domain;
+
+public interface A {
+    int i=0;
+}
---
+++ src/main/java/cn/itcast/travel/domain/Name.java
@@ -0,0 +1,13 @@
+package cn.itcast.travel.domain;
+
+public   class Name extends Category implements A{
+
+
+    public void show(){
+        System.out.println(i);
+    }
+    public static void main(String[] args) {
+      new Name().show();
+    }
+
+}
